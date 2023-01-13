ce6ee4fc833d7fd72ad6631622eed7fe2335c423
Юлия Степанюк <jouristika.artproject@gmail.com>
2023-01-13 22:49:46 +0500

    Commit with docstrings

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from random import randint
 
 
+from grafic_arts.start_game_banner import run_screensaver
+
+
 def attack(char_name: str, char_class: str) -> str:
+    """Функция дает результат тренировок: атака."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс противнику урон, равный '
                 f'{5 + randint(3, 5)}')
@@ -14,6 +18,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Функция дает результат тренировок: защита."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} ед. урона')
     if char_class == 'mage':
@@ -23,6 +28,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Функция дает результат тренировок: спец навыки."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
                 f'«Выносливость {80 + 25}»')
@@ -33,6 +39,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Функция дает пользователю выбрать действие для персонажа."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — великий мастер ближнего боя.')
     if char_class == 'mage':
@@ -57,6 +64,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Функция дает пользователю выбрать персонажа."""
     approve_choice = None
     char_class = None
     while approve_choice != 'y':
@@ -79,7 +87,10 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main():
+def main() -> None:
+    """Запуск игры."""
+    if __name__ = '__main__'
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
--- temp.py
+++ temp.py
@@ -1,5 +1,2 @@
-def we_crash_all(name: str) -> str:
-    return 'Привет, ' + name + ', мы всё сломали!'
+print(print.__doc__)
 
-
-print(we_crash_all('Ann'))
